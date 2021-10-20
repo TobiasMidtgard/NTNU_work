c9fdf1bb16165af701a9db5bd1a310d94951c079
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2021-10-20 10:13:41 +0100

    added something

--- IMAT1001/plotting_example.py
+++ IMAT1001/plotting_example.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 1, 1000)
-y_1 = np.exp(x)
-y_2 = 2*x - 1
+x = np.linspace(-1, 4, 1000)
+y_1 = np.sin(x)
+y_2 = np.cos(x)
+idx = np.argwhere(np.diff(np.sign(y_1 - y_2))).flatten()
 
 fig, ax = plt.subplots()
-ax.plot(x, y_1, label=r"$e^x$", color="r")
-ax.plot(x, y_2, label="2x - 1", color="g")
+ax.plot(x, y_1, label=r"$sin(x)$", color="r")
+ax.plot(x, y_2, label=r"$cos(x)$", color="g")
+ax.plot(x[idx], y_1[idx], 'ro')
 
 ax.set_xlabel("x", loc="right")
 ax.set_ylabel("y", loc="top", rotation=0)
@@ -16,3 +18,6 @@ plt.legend()
 
 plt.grid()
 plt.show()
+
+for val in idx:
+    print(x[val])
